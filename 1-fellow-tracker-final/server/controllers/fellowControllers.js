11de362653171c4e3eb7a1922d9561cc00c89828
benspector-mls <ben@marcylabschool.org>
2025-04-04 21:58:35 -0400

    adding consistent error responses

--- 1-fellow-tracker-final/server/controllers/fellowControllers.js
+++ 1-fellow-tracker-final/server/controllers/fellowControllers.js
@@ -17,13 +17,18 @@ const serveFellow = (req, res) => {
   const { id } = req.params;
   const fellow = Fellow.find(Number(id));
 
-  if (!fellow) return res.status(404).send(`No fellow with the id ${id}`);
+  if (!fellow) return res.status(404).send({ message: `No fellow with the id ${id}` });
   res.send(fellow);
 };
 
 // Create
 const createFellow = (req, res) => {
   const { fellowName } = req.body; // The POST request body will be an object: `{ fellowName: 'name' }`
+  if (!fellowName) {
+    // 400 means "invalid request"
+    return res.status(400).send({ message: "Invalid Name" });
+  }
+
   const newFellow = Fellow.create(fellowName);
   res.send(newFellow);
 };
@@ -31,10 +36,16 @@ const createFellow = (req, res) => {
 // Update
 const updateFellow = (req, res) => {
   const { fellowName } = req.body;
+
+  if (!fellowName) {
+    // 400 means "invalid request"
+    return res.status(400).send({ message: "Invalid Name" });
+  }
+
   const { id } = req.params;
   const updatedFellow = Fellow.editName(Number(id), fellowName);
   // sendStatus sends just the status with no message body
-  if (!updatedFellow) return res.sendStatus(404);
+  if (!updatedFellow) return res.status(404).send({ message: `No fellow with the id ${id}` });
   res.send(updatedFellow);
 }
 
@@ -42,8 +53,12 @@ const updateFellow = (req, res) => {
 const deleteFellow = (req, res) => {
   const { id } = req.params;
   const didDelete = Fellow.delete(Number(id));
-  const statusCode = didDelete ? 204 : 404;
-  res.sendStatus(statusCode);
+
+  if (!didDelete) {
+    return res.status(404).send({ message: `No fellow with the id ${id}` });
+  }
+  // 204 means "no content" - the request was successful but there's no content to send back
+  res.sendStatus(204);
 }
 
 module.exports = {
